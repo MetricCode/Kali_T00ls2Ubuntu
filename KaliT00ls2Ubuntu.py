168dc985159f2194a2028e02f896ccf5229cbf81
M3tr1c_r00t <99975622+MetricCode@users.noreply.github.com>
2023-01-27 23:53:08 +0300

    Added wordlists set-up

--- KaliT00ls2Ubuntu.py
+++ KaliT00ls2Ubuntu.py
@@ -7,6 +7,7 @@
 
 
 import os
+import time
 import sys, traceback
 import subprocess
 from termcolor import colored
@@ -49,7 +50,7 @@ def check_tools(all_tools):
         if strout == not_installed:
             awaiting_install.append(new_var)
     print("The following tools are not installed: ")
-    os.system("sleep 2")
+    time.sleep(2)
     print(awaiting_install)
 
 def install_tools():
@@ -60,7 +61,7 @@ def install_tools():
         for p in uniq_val:
             print(f"Installing {p}")
             os.system(f"sudo snap install {p} -y")
-            os.system("sleep 2")
+            time.sleep(2)
 
     remaining_tools = [b for b in awaiting_install if b not in uniq_val]
 
@@ -69,10 +70,11 @@ def install_tools():
         print(colored(f"Installing {i} ...","red"))
         print(" ")
         os.system(f"sudo apt install {i} -y")
-        os.system("sleep 2")
+        time.sleep(2)
         print(" ")
     print(colored("Finishing up the installation process...","green"))
-    os.system("sleep 3 |echo '...' ")
+    time.sleep(2)
+    print("...")
     print(" ")
     print("And Done! :)")
     
@@ -84,15 +86,40 @@ def create_banner(banner):
 def upgrade_tools():
    want_up = input(colored("Would you like to upgrade your system's tools? (y/n)","red",attrs=["bold"]))
    if want_up == "y":
-       os.system("sleep 1.5")
+       time.sleep(2)
        print(" ")
        print(colored("Upating is in progress. This might take a while... :)","green"))
        print(" ")
        os.system("sudo apt upgrade -y")
    else:
        print(colored("Exiting the tool installer...","red",attrs=["bold"]))
-       os.system("sleep 1")
+       time.sleep(1)
        exit()
+
+def set_wordlists():
+    dirbuster_wordlists = ['apache-user-enum-1.0.txt','apache-user-enum-2.0.txt','directory-list-1.0.txt','directory-list-2.3-big.txt','directory-list-2.3-medium.txt','directory-list-2.3-small.txt','directory-list-lowercase-2.3-big.txt','directory-list-lowercase-2.3-medium.txt','directory-list-lowercase-2.3-small.txt']
+    dns_wordlists = ['bitquark-subdomains-top100000.txt', 'combined_subdomains.txt', 'deepmagic.com-prefixes-top500.txt', 'deepmagic.com-prefixes-top50000.txt', 'dns-Jhaddix.txt', 'fierce-hostlist.txt', 'italian-subdomains.txt', 'namelist.txt', 'shubs-stackoverflow.txt', 'shubs-subdomains.txt', 'sortedcombined-knock-dnsrecon-fierce-reconng.txt', 'subdomains-top1million-110000.txt', 'subdomains-top1million-20000.txt', 'subdomains-top1million-5000.txt', 'tlds.txt']
+    print(colored("Setting up your wordlists...","red",attrs=['bold']))
+
+    os.system("mkdir /usr/share/wordlists")
+    os.system("git clone https://github.com/v0re/dirb /usr/share/wordlists/set1")
+    os.system("mv /usr/share/wordlists/set1/wordlists /usr/share/wordlists/dirb")
+    os.system("rm -r /usr/share/wordlists/set1")
+
+    os.system("mkdir /usr/share/wordlists/dirbuster")
+    for i in dirbuster_wordlists:
+        os.system(f"wget https://hackbbs.org/wordlists/dirbuster/{i} -O /usr/share/wordlists/dirbuster/{i}")
+    
+    print(colored("Now setting sub-domain wordlists :)","red",attrs['bold']))
+    os.system("mkdir /usr/share/wordlists/dns")
+    for i in dns_wordlists:
+        os.system(f"wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/{i} -O /usr/share/wordlists/dns/{i}")
+
+    print(colored("Setting up rockyou wordlists...","red",attrs=['bold']))
+    os.system("wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/rockyou.txt.tar.gz -O /usr/share/wordlists/rockyou.txt.tar.gz")
+    os.system("tar -xvf /usr/share/wordlists/rockyou.txt.tar.gz")
+    time.sleep(1)
+   print(colored("Done!","green",attrs=['bold'])) 
 
 
 def main():
@@ -102,7 +129,7 @@ def main():
     print(colored("Welcome to the M3tr1c_T00l installer for ubuntu users!","red",attrs=["bold"]))
     print(" ")
     print(colored("Hope you enjoy the experience! :)","green",attrs=["bold"]))
-    os.system("sleep 1")
+    time.sleep(1)
     print(" ")
     my_sys_updater = input(colored("Is your system updated? (y/n)","red",attrs=["bold"]))
     if my_sys_updater == "y":
@@ -111,7 +138,7 @@ def main():
     else:
         print(" ")
         print(colored("Updating the system ...","green",attrs=["bold"]))
-        os.system("sleep 2")
+        time.sleep(2)
         os.system("sudo apt-get update -y")
         print(colored("Updating complete!","green",attrs=["bold"]))
 
@@ -123,7 +150,7 @@ def main():
         print(" ")
         print(colored("Installing snap ...","red",attrs=["bold"]))
         print(" ")
-        os.system("sleep 2")
+        time.sleep(2)
         os.system("sudo apt install snapd -y")
         print(colored("Snap has been installed :)","green",attrs=["bold"]))
 
@@ -135,14 +162,23 @@ def main():
         if install_choice == "y":
             install_tools()
             upgrade_tools()
+            get_wordlists = print(colored("Would you like to set up your wordlists? (y/n)","green",attrs=['bold']))
+            if get_wordlists =="y":
+                set_wordlists()
+                time.sleep(1)
+                print(colored("Full setup complete! Thank you for your time!(Feel free to give the creator credit by starring this repository ;))","yellow",attrs=['bold']))
+                print(colored("Thank you for your time!(Feel free to give the creator credit by starring this repository ;))\nExiting the tool installer...","red",attrs=["bold"]))
+                time.sleep(2)
+                os.system("exit")
+                
         else:
-            print(colored("Exiting the tool installer...","red",attrs=["bold"]))
-            os.system("sleep 2")
+            print(colored("Thank you for your time!(Feel free to give the creator credit by starring this repository ;))\nExiting the tool installer...","red",attrs=["bold"]))
+            time.sleep(2)
             os.system("exit")
             
     else:
-        print(colored("Exiting the tool installer...","red",attrs=["bold"]))
-        os.system("sleep 1")
+        print(colored("Thank you for your time!(Feel free to give the creator credit by starring this repository ;))\nExiting the tool installer...","red",attrs=["bold"]))
+        time.sleep(2)
         os.system("exit")
 
 
