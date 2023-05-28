d7fb85ffed9f5d400cc181bde65eea55c66d44b3
M3tr1c_r00t <99975622+MetricCode@users.noreply.github.com>
2023-05-28 03:02:00 +0300

    Update KaliT00ls2Ubuntu.py

--- KaliT00ls2Ubuntu.py
+++ KaliT00ls2Ubuntu.py
@@ -7,25 +7,17 @@
 
 
 import os
+import fade
 import time
 import sys, traceback
 import subprocess
 from termcolor import colored
 
-
 banner = """
- _______  ______ _________ _______  __    _______   _________ _______  _______  _              __    _        _______ _________ _______  _        _        ______   _______
-(       )/ ___  \\__   __/(  ____ )/  \  (  ____ \  \__   __/(  __   )(  __   )( \           /  \  ( (    /|(  ____ \\__   __/(  ___  )( \      ( \      / ___  \ (  ____ )
-| () () |\/   \  \  ) (   | (    )|\/) ) | (    \/     ) (   | (  )  || (  )  || (           \/) ) |  \  ( || (    \/   ) (   | (   ) || (      | (      \/   \  \| (    )|
-| || || |   ___) /  | |   | (____)|  | | | |           | |   | | /   || | /   || |             | | |   \ | || (_____    | |   | (___) || |      | |         ___) /| (____)|
-| |(_)| |  (___ (   | |   |     __)  | | | |           | |   | (/ /) || (/ /) || |             | | | (\ \) |(_____  )   | |   |  ___  || |      | |        (___ ( |     __)
-| |   | |      ) \  | |   | (\ (     | | | |           | |   |   / | ||   / | || |             | | | | \   |      ) |   | |   | (   ) || |      | |            ) \| (\ (
-| )   ( |/\___/  /  | |   | ) \ \____) (_| (____/\     | |   |  (__) ||  (__) || (____/\     __) (_| )  \  |/\____) |   | |   | )   ( || (____/\| (____/\/\___/  /| ) \ \__
-|/     \|\______/   )_(   |/   \__/\____/(_______/_____)_(   (_______)(_______)(_______/_____\____/|/    )_)\_______)   )_(   |/     \|(_______/(_______/\______/ |/   \__/
-                                                 (_____)                               (_____)                                                                             """
-
-
-
+┬┌─┌─┐┬  ┬┌┬┐┌─┐┌─┐┬  ┌─┐    ┬ ┬┌┐ ┬ ┬┌┐┌┌┬┐┬ ┬
+├┴┐├─┤│  │ │ │ ││ ││  └─┐ -- │ │├┴┐│ ││││ │ │ │
+┴ ┴┴ ┴┴─┘┴ ┴ └─┘└─┘┴─┘└─┘    └─┘└─┘└─┘┘└┘ ┴ └─┘
+"""
 all_tools = ['plocate','vim', 'python2','hashcat','john', 'hydra', 'wireshark', 'nmap', 'gobuster', 'dirsearch', 'ffuf', 'steghide', 'fcrackzip', 'sqlmap', 'binwalk','whois', 'ruby-full', 'wafw00f', 'python2', 'gdb', 'openvpn', 'p7zip', 'pdfcrack',  'rdesktop', 'sqlitebrowser', 'tcpdump','hashid']
 snap_tools = ['searchsploit','metasploit-framework', 'crackmapexec', 'enum4linux','ghidra']
 checked_tools = ['net-tools', 'python3-impacket', 'freerdp2-dev', 'libapache2-mod-php', 'default-libmysqlclient-dev', 'openssh-client']
@@ -55,7 +47,10 @@ def check_tools(all_tools):
     time.sleep(2)
     print(awaiting_install)
 
+
 def install_tools():
+
+    # seperating snap tools from the tools awaiting installation list...
     uniq_val = [y for y in awaiting_install if y in snap_tools]
     if uniq_val == None:
         pass
@@ -65,41 +60,35 @@ def install_tools():
             os.system(f"sudo snap install {p}")
             time.sleep(2)
 
+    # tools to install with apt seperated from the snap tools after being installed...
     remaining_tools = [b for b in awaiting_install if b not in uniq_val]
 
     for i in remaining_tools:
-        print(" ")
-        print(colored(f"Installing {i} ...","red"))
-        print(" ")
+        print(colored(f"\nInstalling {i} ...\n","red"))
         os.system(f"sudo apt install {i} -y")
         time.sleep(2)
-        print(" ")
+
+    # installing this tools without checking if they are installed...
     for i in checked_tools:
-        print(" ")
-        print(colored(f"Installing {i} ...","red"))
-        print(" ")
-        os.system(f"sudo apt install {i} -y")
-        time.sleep(2)
-        print(" ")
+        print(colored(f"\nInstalling {i} ...","red"))
+        os.system(f"\nsudo apt install {i} -y")
+        time.sleep(1)
         
     print(colored("Finishing up the installation process...","green"))
-    time.sleep(2)
-    print("...")
-    print(" ")
-    print("And Done! :)")
+    time.sleep(1)
+    print("..."+ "\nAnd Done! :)")
     
 
 def create_banner(banner):
-    colored_banner = colored(banner,"green",attrs=["bold"])
-    print(colored_banner)
+    colored_banner = fade.pinkred(banner)
+    print(f"{colored_banner}\n" + fade.purpleblue("Created by M3tr1c_r00t...\n\nFollow on Twitter: m3tr1c_r00t\nThank you!"))
+
    
 def upgrade_tools():
    want_up = input(colored("Would you like to upgrade your system's tools? (y/n)","red",attrs=["bold"]))
    if want_up == "y":
        time.sleep(2)
-       print(" ")
        print(colored("Upating is in progress. This might take a while... :)","green"))
-       print(" ")
        os.system("sudo apt upgrade -y")
    else:
        print(colored("Exiting the tool installer...","red",attrs=["bold"]))
@@ -117,40 +106,36 @@ def set_wordlists():
 def main():
     create_banner(banner)
     check_root()
-    print(" ")
-    print(colored("Welcome to the M3tr1c_T00l installer for ubuntu users!","red",attrs=["bold"]))
-    print(" ")
-    print(colored("Hope you enjoy the experience! :)","green",attrs=["bold"]))
-    time.sleep(1)
-    print(" ")
     my_sys_updater = input(colored("Is your system updated? (y/n)","red",attrs=["bold"]))
     if my_sys_updater == "y":
-        print(" ")
         pass
-    else:
-        print(" ")
+    elif my_sys_updater == "n":
         print(colored("Updating the system ...","green",attrs=["bold"]))
         time.sleep(2)
         os.system("sudo apt-get update -y")
         print(colored("Updating complete!","green",attrs=["bold"]))
+    else:
+        print("Invalid choice")
+    
 
     checking_snap_installation = input(colored("Do you have snap installed? (y/n)","green",attrs= ["bold"]))
     if checking_snap_installation == "y":
-        print(" ")
         pass
-    else:
-        print(" ")
-        print(colored("Installing snap ...","red",attrs=["bold"]))
-        print(" ")
+    elif checking_snap_installation == "n":
+        print(colored("\nInstalling snap ...","red",attrs=["bold"]))
         time.sleep(2)
         os.system("sudo apt install snapd -y")
         print(colored("Snap has been installed :)","green",attrs=["bold"]))
+    else:
+        print("Invalid choice")
 
 
     choice = input(colored("Would you like to check your tools and install missing ones? (y/n)","red",attrs=["bold"]))
+
     if choice == "y":
         check_tools(all_tools)
         install_choice = input(colored("Would you like to install the missing tools? (y/n)","green",attrs=["bold"]))
+
         if install_choice == "y":
             install_tools()
             upgrade_tools()
@@ -158,18 +143,18 @@ def main():
             if get_wordlists =="y":
                 set_wordlists()
                 time.sleep(1)
-                print(colored("Full setup complete! Thank you for your time!(Feel free to give the creator credit by starring this repository ;))","yellow",attrs=['bold']))
-                print(colored("Thank you for your time!(Feel free to give the creator credit by starring this repository ;))\nExiting the tool installer...","red",attrs=["bold"]))
+                print(colored("Full setup complete! ","yellow",attrs=['bold']))
+                print(colored("Exiting the tool installer...","red",attrs=["bold"]))
                 time.sleep(2)
                 os.system("exit")
                 
         else:
-            print(colored("Thank you for your time!(Feel free to give the creator credit by starring this repository ;))\nExiting the tool installer...","red",attrs=["bold"]))
+            print(colored("Exiting the tool installer...","red",attrs=["bold"]))
             time.sleep(2)
             os.system("exit")
             
     else:
-        print(colored("Thank you for your time!(Feel free to give the creator credit by starring this repository ;))\nExiting the tool installer...","red",attrs=["bold"]))
+        print(colored("Exiting the tool installer...","red",attrs=["bold"]))
         time.sleep(2)
         os.system("exit")
 
@@ -178,5 +163,4 @@ def main():
 try:
     main()
 except KeyboardInterrupt:
-    print(" ")
-    print(colored("\nLeaving program. Goodbye :)","blue"))
+    print(colored("Exiting the tool installer...","blue"))
